1262ef0345b68e27f85a6c4859cea48ce9c001de
Jack Frost <kodeandplay@gmail.com>
2018-09-18 21:34:06 -0700

    another commit

---
+++ db/index.js
@@ -0,0 +1,25 @@
+const {Pool} = require('pg')
+const creds = require('../creds.json')
+
+const config = {
+  user: creds.user,
+  password: creds.password,
+  database: creds.database,
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 30000
+}
+
+const pool = new Pool(config)
+
+module.exports = {
+  query: (text, params, cb) => {
+    const start = Date.now()
+    return pool.query(text, params, (err, res) => {
+      const duration = Date.now() - start
+      //console.log('executed query', {text, duration, rows: res.rowCount})
+      cb(err, res)
+    })
+  }
+}
